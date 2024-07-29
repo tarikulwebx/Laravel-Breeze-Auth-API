d5d96cb9e84b40de597ca3a9b736b2eff963dc07
Md. Tarikul Islam <tarikulwebx@gmail.com>
2024-07-29 14:50:26 +0600

    Auth completed

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 
 class VerifyEmailController extends Controller
@@ -12,7 +13,7 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    public function __invoke(EmailVerificationRequest $request): JsonResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
             // return redirect()->intended(
--- routes/api_auth.php
+++ routes/api_auth.php
@@ -8,26 +8,33 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+// register new user
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->name('register');
 
+// login user
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->name('login');
 
+// sent password reset link
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->name('password.email');
 
+// reset password
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
     ->name('password.store');
 
+// verify email by token
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
     ->middleware(['auth:sanctum', 'signed', 'throttle:6,1'])
     ->name('verification.verify');
 
+// sent email verification link
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
     ->middleware(['auth:sanctum', 'throttle:6,1'])
     ->name('verification.send');
 
+// logout user
 Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth:sanctum')
     ->name('logout');
